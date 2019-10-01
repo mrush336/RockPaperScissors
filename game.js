d063facef9b1207f93000abc425e23419ed44504
Matthew Rushford <49460419+mrush336@users.noreply.github.com>
2019-10-01 08:57:12 -0400

    Got the main js working. Still need html and css design work.

---
+++ game.js
@@ -0,0 +1,30 @@
+//Rock Paper Scissors game
+
+//Player selection function
+function playerSelect(choice){
+    choice = prompt('Enter your choice: rock | paper | scissors');
+    return choice;
+}
+
+//Computer selection function
+function compSelect(choice){
+    let choices = ['rock', 'paper', 'scissors'];
+    choice = choices[Math.floor(Math.random()*3)];
+    return choice;
+}
+
+//Main game function
+function playRound(playerSelect, compSelect){
+
+    if (playerSelect.toLowerCase() === compSelect){
+        alert(`The computer chose, ${compSelect} it is a tie!`);
+    }else if (playerSelect.toLowerCase() === 'rock' && compSelect === 'scissors'
+        || playerSelect.toLowerCase() === 'paper' && compSelect === 'rock'
+        || playerSelect.toLowerCase() === 'scissors' && compSelect === 'paper'){
+        alert(`The computer chose ${compSelect}, You won!!`);}
+    else{
+    alert(`The computer chose ${compSelect}, You lost.`);
+    }
+}
+//Call to start the game
+playRound(playerSelect(), compSelect());
