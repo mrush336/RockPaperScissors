b9f6bb23c2a7c0b7a45efcd601b466995925a313
Matthew Rushford <49460419+mrush336@users.noreply.github.com>
2019-10-01 16:35:39 -0400

    Added for loop to main function call.

--- game.js
+++ game.js
@@ -2,34 +2,37 @@
 
 //Player selection function
 function playerSelect(choice){
-    choice = prompt('Enter your choice: rock | paper | scissors');
-    return choice;
+  choice = prompt('Enter your choice: rock | paper | scissors');
+  return choice;
 }
 
 //Computer selection function
 function compSelect(choice){
-    let choices = ['rock', 'paper', 'scissors'];
-    choice = choices[Math.floor(Math.random()*3)];
-    return choice;
+  let choices = ['rock', 'paper', 'scissors'];
+  choice = choices[Math.floor(Math.random()*3)];
+  return choice;
 }
 
 //Main game function
 function playRound(playerSelect, compSelect){
 
-    if (playerSelect.toLowerCase() === compSelect){
-        alert(`The computer chose, ${compSelect} it is a tie!`);
-    }else if (playerSelect.toLowerCase() === 'rock' && compSelect === 'scissors'
-        || playerSelect.toLowerCase() === 'paper' && compSelect === 'rock'
-        || playerSelect.toLowerCase() === 'scissors' && compSelect === 'paper'){
-        alert(`The computer chose ${compSelect}, You won!!`);}
-    else{
+  if (playerSelect.toLowerCase() === compSelect){
+    alert(`The computer chose, ${compSelect} it is a tie!`);
+  }else if (playerSelect.toLowerCase() === 'rock' && compSelect === 'scissors'
+    || playerSelect.toLowerCase() === 'paper' && compSelect === 'rock'
+    || playerSelect.toLowerCase() === 'scissors' && compSelect === 'paper'){
+    alert(`The computer chose ${compSelect}, You won!!`);
+  }else{
     alert(`The computer chose ${compSelect}, You lost.`);
-    }
+  }
 }
+
 
 //Call to start the game
 if (prompt('Do you want to play a game?') === 'yes'){ 
+  for(i = 0; i < 5; i++){
     playRound(playerSelect(), compSelect());
+  }
 }else{
     confirm('Maybe later then.');
 }
